fa4acabc130b47fb6e4d066851e5caf0d7552a2b
nayon-kumar <nayon.cse55@gmail.com>
2026-04-06 22:49:27 +0600

    All books section design started

---
+++ src/components/HomePage/AllBooks.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllBooks = () => {
+  return (
+    <div>
+      <p>All books</p>
+    </div>
+  );
+};
+
+export default AllBooks;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../components/HomePage/Banner";
+import AllBooks from "../../components/HomePage/AllBooks";
 
 const HomePage = () => {
   return (
     <div className="max-w-7xl mx-auto">
       <Banner />
+      <AllBooks />
     </div>
   );
 };
