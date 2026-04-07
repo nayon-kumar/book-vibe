95843142b7566184509cc9f77eaeffbfd5624484
nayon-kumar <nayon.cse55@gmail.com>
2026-04-07 22:04:20 +0600

    Read book card designed

--- src/pages/BookPage/BookPage.jsx
+++ src/pages/BookPage/BookPage.jsx
@@ -1,12 +1,42 @@
 import React, { useContext } from "react";
 import { BookContext } from "../../context/MyContext";
+import ReadBookCard from "../../ui/ReadBookCard";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const BookPage = () => {
-  const { readBooks } = useContext(BookContext);
+  const { readBooks, wishlistBooks } = useContext(BookContext);
+  console.log("Read", readBooks);
+  console.log("Wish", wishlistBooks);
 
   return (
     <div className="max-w-7xl mx-auto">
-      <p>Book page</p>
+      <div className="text-center bg-[#131313]/5 rounded-2xl py-7 ">
+        <h3 className="font-bold text-2xl">Books</h3>
+      </div>
+      <div className="mt-15">
+        <Tabs>
+          <TabList>
+            <Tab>
+              <p className="font-bold">Read Books</p>
+            </Tab>
+            <Tab>
+              <p className="font-bold">Wishlist Books</p>
+            </Tab>
+          </TabList>
+
+          <TabPanel>
+            <div className="flex flex-col gap-6">
+              {readBooks.map((readBook) => (
+                <ReadBookCard key={readBook.bookId} readBook={readBook} />
+              ))}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
+      </div>
     </div>
   );
 };
---
+++ src/ui/ReadBookCard.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { FaUserGroup } from "react-icons/fa6";
+import { GrDocumentText } from "react-icons/gr";
+
+const ReadBookCard = ({ readBook }) => {
+  console.log(readBook);
+  return (
+    <div className="border-[#131313]/15 border-2 rounded-2xl p-6 grid md:grid-cols-8 gap-8 shadow-lg hover:shadow-xl">
+      <div className="bg-[#131313]/5 flex items-center justify-center py-8 w-full rounded-2xl md:col-span-3 lg:col-span-2">
+        <img
+          className="h-43 mx-auto"
+          src={readBook.image}
+          alt={readBook.bookName}
+        />
+      </div>
+      <div className="md:col-span-5 lg:col-span-6">
+        <h2 className="text-[#131313] font-bold text-xl md:text-2xl">
+          {readBook.bookName}
+        </h2>
+        <p className="text-[#131313]/80 my-4 font-medium">
+          By : {readBook.author}
+        </p>
+        <div className="flex flex-wrap items-center gap-4">
+          <div>
+            <p className="font-bold">Tag</p>
+          </div>
+          <div className="flex gap-3 items-center">
+            {readBook.tags.map((tag, index) => (
+              <p
+                className="text-[#23BE0A] font-medium bg-[#23BE0A]/5 px-5 rounded-full py-1.5 "
+                key={index}
+              >
+                #{tag}
+              </p>
+            ))}
+          </div>
+          <div>
+            <p>Year of Publishing: 1924</p>
+          </div>
+        </div>
+        <div className="flex gap-4 mt-4">
+          <div className="flex items-center gap-2 text-[#131313]/60">
+            <FaUserGroup />
+            <p>Publisher: Scribner</p>
+          </div>
+          <div className="flex items-center gap-2 text-[#131313]/60">
+            <GrDocumentText />
+            <p>Page 192</p>
+          </div>
+        </div>
+        <div className="flex flex-wrap gap-3 mt-4 border-t-2 border-[#131313]/15 pt-4">
+          <button className="rounded-full px-5 py-2.5 bg-[#328EFF]/15 text-[#328EFF] ">
+            Category: Classic
+          </button>
+          <button className="rounded-full px-5 py-2.5 bg-[#FFAC33]/15 text-[#FFAC33] ">
+            Rating: 4.5
+          </button>
+          <button className="rounded-full px-5 py-2.5 bg-[#23BE0A] text-white cursor-pointer ">
+            View Details
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReadBookCard;
