44f62d6ed16660ded9639cf8681881127c37f95e
nayon-kumar <nayon.cse55@gmail.com>
2026-04-08 13:31:50 +0600

    Scroll issue fixed

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,6 +1,7 @@
 import { Link, useLoaderData, useParams } from "react-router";
 import { BookContext } from "../../context/MyContext";
 import { useContext } from "react";
+import ScrollToTop from "../../components/ScrollToTop/ScrollToTop";
 
 const BookDetails = () => {
   const books = useLoaderData();
@@ -20,75 +21,78 @@ const BookDetails = () => {
   );
 
   return (
-    <div className="flex flex-wrap items-center justify-center gap-10 mt-25 max-w-7xl mx-auto">
-      <div className="md:flex-1 bg-[#131313]/5 py-10 md:py-20 w-full text-center rounded-2xl">
-        <img
-          className="h-100 mx-auto hover:scale-110 transition-all duration-500 ease-in-out"
-          src={expectedBook.image}
-          alt={expectedBook.bookName}
-        />
-      </div>
-      <div className="md:flex-1">
-        <h2 className="text-[#131313] font-bold text-2xl md:text-3xl">
-          {expectedBook.bookName}
-        </h2>
-        <p className="text-[#131313]/80 mt-4 border-b-2 pb-8 border-[#131313]/15 font-medium ">
-          By : {expectedBook.author}
-        </p>
-        <p className="mt-4 border-b-2 border-[#131313]/15 pb-4 font-medium text-[#131313]/80 ">
-          {expectedBook.category}
-        </p>
-        <p className="mt-6 text-justify ">
-          <span className="font-bold">Review : </span>
-          <span className="text-[#131313]/70">{expectedBook.review}</span>
-        </p>
-        <div className="flex items-center gap-4 mt-6 border-b-2 border-[#131313]/15 pb-6">
-          <div>
-            <p className="font-bold">Tag</p>
-          </div>
-          <div className="flex gap-3 items-center">
-            {expectedBook.tags.map((tag, index) => (
-              <p
-                className="text-[#23BE0A] font-medium bg-[#23BE0A]/5 px-5 rounded-full py-1.5 "
-                key={index}
-              >
-                #{tag}
-              </p>
-            ))}
-          </div>
+    <div>
+      <ScrollToTop />
+      <div className="flex flex-wrap items-center justify-center gap-10 mt-25 max-w-7xl mx-auto">
+        <div className="md:flex-1 bg-[#131313]/5 py-10 md:py-20 w-full text-center rounded-2xl">
+          <img
+            className="h-100 mx-auto hover:scale-110 transition-all duration-500 ease-in-out"
+            src={expectedBook.image}
+            alt={expectedBook.bookName}
+          />
         </div>
-        <div className="mt-6 flex gap-8">
-          <div className="text-[#131313]/70 space-y-3">
-            <p>Number of Pages: </p>
-            <p>Publisher:</p>
-            <p>Year of Publishing:</p>
-            <p>Rating:</p>
+        <div className="md:flex-1">
+          <h2 className="text-[#131313] font-bold text-2xl md:text-3xl">
+            {expectedBook.bookName}
+          </h2>
+          <p className="text-[#131313]/80 mt-4 border-b-2 pb-8 border-[#131313]/15 font-medium ">
+            By : {expectedBook.author}
+          </p>
+          <p className="mt-4 border-b-2 border-[#131313]/15 pb-4 font-medium text-[#131313]/80 ">
+            {expectedBook.category}
+          </p>
+          <p className="mt-6 text-justify ">
+            <span className="font-bold">Review : </span>
+            <span className="text-[#131313]/70">{expectedBook.review}</span>
+          </p>
+          <div className="flex items-center gap-4 mt-6 border-b-2 border-[#131313]/15 pb-6">
+            <div>
+              <p className="font-bold">Tag</p>
+            </div>
+            <div className="flex gap-3 items-center">
+              {expectedBook.tags.map((tag, index) => (
+                <p
+                  className="text-[#23BE0A] font-medium bg-[#23BE0A]/5 px-5 rounded-full py-1.5 "
+                  key={index}
+                >
+                  #{tag}
+                </p>
+              ))}
+            </div>
           </div>
-          <div className="space-y-3 font-semibold text-[#131313] ">
-            <p>281</p>
-            <p>J.B Lippincott & Co.</p>
-            <p>1960</p>
-            <p>4.8</p>
+          <div className="mt-6 flex gap-8">
+            <div className="text-[#131313]/70 space-y-3">
+              <p>Number of Pages: </p>
+              <p>Publisher:</p>
+              <p>Year of Publishing:</p>
+              <p>Rating:</p>
+            </div>
+            <div className="space-y-3 font-semibold text-[#131313] ">
+              <p>281</p>
+              <p>J.B Lippincott & Co.</p>
+              <p>1960</p>
+              <p>4.8</p>
+            </div>
           </div>
-        </div>
-        <div className="mt-8 flex flex-wrap items-center justify-center md:justify-normal gap-4">
-          <button
-            onClick={() => addToRead(expectedBook)}
-            className="btn btn-info text-white"
-            disabled={isExistInRead}
-          >
-            {isExistInRead ? "Marked as Read" : "Mark as Read"}
-          </button>
-          <button
-            onClick={() => addToWishlist(expectedBook)}
-            className="btn btn-primary"
-            disabled={isExistInWish}
-          >
-            {isExistInWish ? "Added to Wishlist" : "Add to Wishlist"}
-          </button>
-          <Link to="/" className="btn">
-            Back to Home
-          </Link>
+          <div className="mt-8 flex flex-wrap items-center justify-center md:justify-normal gap-4">
+            <button
+              onClick={() => addToRead(expectedBook)}
+              className="btn btn-info text-white"
+              disabled={isExistInRead}
+            >
+              {isExistInRead ? "Marked as Read" : "Mark as Read"}
+            </button>
+            <button
+              onClick={() => addToWishlist(expectedBook)}
+              className="btn btn-primary"
+              disabled={isExistInWish}
+            >
+              {isExistInWish ? "Added to Wishlist" : "Add to Wishlist"}
+            </button>
+            <Link to="/" className="btn">
+              Back to Home
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/BookPage/BookPage.jsx
+++ src/pages/BookPage/BookPage.jsx
@@ -5,6 +5,7 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import WishBookCard from "../../ui/WishBookCard";
 import { FaBookOpenReader } from "react-icons/fa6";
+import ScrollToTop from "../../components/ScrollToTop/ScrollToTop";
 
 const BookPage = () => {
   const { readBooks, wishlistBooks } = useContext(BookContext);
@@ -55,138 +56,141 @@ const BookPage = () => {
   }, [sortingType, readBooks, wishlistBooks]);
 
   return (
-    <div className="max-w-7xl mx-auto">
-      <div className="text-center bg-[#131313]/5 rounded-2xl py-7 ">
-        <h3 className="font-bold text-2xl">Books</h3>
-      </div>
-      <div className="text-center mt-10">
-        <div className="dropdown dropdown-bottom">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn m-1 bg-[#23BE0A] text-white"
-          >
-            Sort By 🡻
-          </div>
-          <ul
-            tabIndex="-1"
-            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
-          >
-            <li onClick={() => setSortingType("")}>
-              <a>Default</a>
-            </li>
-            <li onClick={() => setSortingType("pages-low-to-high")}>
-              <a>Pages: Low to High</a>
-            </li>
-            <li onClick={() => setSortingType("rating-low-to-high")}>
-              <a>Rating: Low to High</a>
-            </li>
-
-            <li onClick={() => setSortingType("pages-high-to-low")}>
-              <a>Pages: High to Low</a>
-            </li>
-            <li onClick={() => setSortingType("rating-high-to-low")}>
-              <a>Rating: High to Low</a>
-            </li>
-          </ul>
+    <div>
+      <ScrollToTop />
+      <div className="max-w-7xl mx-auto">
+        <div className="text-center bg-[#131313]/5 rounded-2xl py-7 ">
+          <h3 className="font-bold text-2xl">Books</h3>
         </div>
-      </div>
-      <div className="mt-8">
-        <Tabs>
-          <TabList>
-            <Tab>
-              <p className="font-bold">Read Books</p>
-            </Tab>
-            <Tab>
-              <p className="font-bold">Wishlist Books</p>
-            </Tab>
-          </TabList>
+        <div className="text-center mt-10">
+          <div className="dropdown dropdown-bottom">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn m-1 bg-[#23BE0A] text-white"
+            >
+              Sort By 🡻
+            </div>
+            <ul
+              tabIndex="-1"
+              className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+            >
+              <li onClick={() => setSortingType("")}>
+                <a>Default</a>
+              </li>
+              <li onClick={() => setSortingType("pages-low-to-high")}>
+                <a>Pages: Low to High</a>
+              </li>
+              <li onClick={() => setSortingType("rating-low-to-high")}>
+                <a>Rating: Low to High</a>
+              </li>
 
-          <TabPanel>
-            {readBooks.length === 0 ? (
-              // If read books list is empty
-              <>
-                <div className="flex flex-col items-center justify-center py-20">
-                  <FaBookOpenReader className="text-gray-400" size={100} />
-                  <p className="mt-5 text-gray-400 text-center">
-                    Your read books list is empty. Go to Home page for add books
-                    in your read books list.
-                  </p>
-                </div>
-              </>
-            ) : (
-              <>
-                {sortingType === "" ? (
-                  <>
-                    {/* Default read books data */}
-                    <div className="flex flex-col gap-8 mt-10">
-                      {readBooks.map((readBook) => (
-                        <ReadBookCard
-                          key={readBook.bookId}
-                          readBook={readBook}
-                        />
-                      ))}
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    {/* Sorted read books data */}
-                    <div className="flex flex-col gap-8 mt-10">
-                      {sortedReadBooks.map((readBook) => (
-                        <ReadBookCard
-                          key={readBook.bookId}
-                          readBook={readBook}
-                        />
-                      ))}
-                    </div>
-                  </>
-                )}
-              </>
-            )}
-          </TabPanel>
-          <TabPanel>
-            {wishlistBooks.length === 0 ? (
-              // If wishlist books is empty
-              <>
-                <div className="flex flex-col items-center justify-center py-20">
-                  <FaBookOpenReader className="text-gray-400" size={100} />
-                  <p className="mt-5 text-gray-400 text-center">
-                    Your wishlist books is empty. Go to Home page for add your
-                    favourite books in your wishlist.
-                  </p>
-                </div>
-              </>
-            ) : (
-              <>
-                {sortingType === "" ? (
-                  <>
-                    {/* Default wishlist data */}
-                    <div className="flex flex-col gap-8 mt-10">
-                      {wishlistBooks.map((wishlistBook) => (
-                        <WishBookCard
-                          key={wishlistBook.bookId}
-                          wishlistBook={wishlistBook}
-                        />
-                      ))}
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    {/* Sorted wishlist data */}
-                    <div className="flex flex-col gap-8 mt-10">
-                      {sortedWishlishBooks.map((wishlistBook) => (
-                        <WishBookCard
-                          key={wishlistBook.bookId}
-                          wishlistBook={wishlistBook}
-                        />
-                      ))}
-                    </div>
-                  </>
-                )}
-              </>
-            )}
-          </TabPanel>
-        </Tabs>
+              <li onClick={() => setSortingType("pages-high-to-low")}>
+                <a>Pages: High to Low</a>
+              </li>
+              <li onClick={() => setSortingType("rating-high-to-low")}>
+                <a>Rating: High to Low</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="mt-8">
+          <Tabs>
+            <TabList>
+              <Tab>
+                <p className="font-bold">Read Books</p>
+              </Tab>
+              <Tab>
+                <p className="font-bold">Wishlist Books</p>
+              </Tab>
+            </TabList>
+
+            <TabPanel>
+              {readBooks.length === 0 ? (
+                // If read books list is empty
+                <>
+                  <div className="flex flex-col items-center justify-center py-20">
+                    <FaBookOpenReader className="text-gray-400" size={100} />
+                    <p className="mt-5 text-gray-400 text-center">
+                      Your read books list is empty. Go to Home page for add
+                      books in your read books list.
+                    </p>
+                  </div>
+                </>
+              ) : (
+                <>
+                  {sortingType === "" ? (
+                    <>
+                      {/* Default read books data */}
+                      <div className="flex flex-col gap-8 mt-10">
+                        {readBooks.map((readBook) => (
+                          <ReadBookCard
+                            key={readBook.bookId}
+                            readBook={readBook}
+                          />
+                        ))}
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      {/* Sorted read books data */}
+                      <div className="flex flex-col gap-8 mt-10">
+                        {sortedReadBooks.map((readBook) => (
+                          <ReadBookCard
+                            key={readBook.bookId}
+                            readBook={readBook}
+                          />
+                        ))}
+                      </div>
+                    </>
+                  )}
+                </>
+              )}
+            </TabPanel>
+            <TabPanel>
+              {wishlistBooks.length === 0 ? (
+                // If wishlist books is empty
+                <>
+                  <div className="flex flex-col items-center justify-center py-20">
+                    <FaBookOpenReader className="text-gray-400" size={100} />
+                    <p className="mt-5 text-gray-400 text-center">
+                      Your wishlist books is empty. Go to Home page for add your
+                      favourite books in your wishlist.
+                    </p>
+                  </div>
+                </>
+              ) : (
+                <>
+                  {sortingType === "" ? (
+                    <>
+                      {/* Default wishlist data */}
+                      <div className="flex flex-col gap-8 mt-10">
+                        {wishlistBooks.map((wishlistBook) => (
+                          <WishBookCard
+                            key={wishlistBook.bookId}
+                            wishlistBook={wishlistBook}
+                          />
+                        ))}
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      {/* Sorted wishlist data */}
+                      <div className="flex flex-col gap-8 mt-10">
+                        {sortedWishlishBooks.map((wishlistBook) => (
+                          <WishBookCard
+                            key={wishlistBook.bookId}
+                            wishlistBook={wishlistBook}
+                          />
+                        ))}
+                      </div>
+                    </>
+                  )}
+                </>
+              )}
+            </TabPanel>
+          </Tabs>
+        </div>
       </div>
     </div>
   );
